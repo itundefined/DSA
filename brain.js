deb15aaa98d2aa7a18c2258c8649c8ff6e84705a
Tushar Sharma (波風ミナト) <tusharsharmaek@gmail.com>
2023-02-07 17:08:15 +0530

    Three sum algorightm is done 100% with O(n^2) from o(n^3)

--- brain.js
+++ brain.js
@@ -4,8 +4,7 @@ const colors = require("colors");
 
 // Given an array containing n integers, and an number and an number denoting a target sum, return an of those two number's index in array which can add to the target sum, and output should be in the same order as you would get the input;
 
-//  Given an array
-
+// Time Complexity of this Algorithm  = O(N);
 const TwoSum = (inputArray = [10, 5, 2, 3, -6, 9, 11], target = 4) => {
   // over Here we may use the one more data structure to just split the time and take time complexity from o(n^2) -> ot o(n)
 
@@ -20,7 +19,7 @@ const TwoSum = (inputArray = [10, 5, 2, 3, -6, 9, 11], target = 4) => {
     // If we find that element in our hashmap thne we can just return
 
     if (hashmap[NumToFind] !== undefined) {
-      console.log(colors.red([hashmap[NumToFind], indexer]));
+      console.log(colors.cyan.bold.italic.underline([hashmap[NumToFind], indexer]));
       return hashmap[NumToFind], indexer;
     }
 
@@ -31,17 +30,33 @@ const TwoSum = (inputArray = [10, 5, 2, 3, -6, 9, 11], target = 4) => {
   }
 };
 
+
 // Given an array containing n integers, and an number  denoting a target sum; Find all distinct integers that can add to the target sum. The number in each triplet should be ordered in ascending order, and triplets should  be ordered too. *Return an empty array if no such triplet exist*
 
-const ThreeSum = (
-  inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 15],
-  target = 18
-) => {
-    
-  
-  for (const element of inputArray) {
+// Time complexity of this algorithm is O(N^2).
+const ThreeSum = (inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 15], target = 18) => {
+
+  let answer = [];
+
+  for (let index = 0; index < inputArray.length - 2; index++) {
+    let lowerValuePointer = index + 1;
+    let higherValuePointer = inputArray.length - 1;
+    let CurrentElement = inputArray[index];
+
+    for (let index2 = index + 1; index2 < inputArray.length; index2++) {
+        let lowerPointerActualValue = inputArray[lowerValuePointer];
+        let higherPointerActualValue = inputArray[higherValuePointer];
+
+        if (CurrentElement + lowerPointerActualValue + higherPointerActualValue > target) higherValuePointer--;
+        
+        else if (CurrentElement + lowerPointerActualValue + higherPointerActualValue < target)  lowerValuePointer++; 
+
+        else if (CurrentElement + lowerPointerActualValue + higherPointerActualValue === target) {
+          answer.push([CurrentElement, lowerPointerActualValue, higherPointerActualValue]);
+          break;}
+      }
   }
 
+  console.log(colors.bold.italic.underline.cyan(answer));
+  return answer;
 };
-
-ThreeSum();
